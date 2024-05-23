7f01cf55dd9e3c5ba28d46f1eab8aaaab24028b4
gssheng <sgs@growthman.cn>
2024-05-23 19:45:48 +0800

    feat: 通过状态管理，完成登录验证

--- components/nav/Bar.vue
+++ components/nav/Bar.vue
@@ -2,7 +2,9 @@
     <nav
       class="border-b border-slate-200 px-5 py-2 flex items-center justify-between"
     >
+    <NuxtLink class="text-lg" :to="`/`">
       <h1 class="text-2xl font-bold">Nuxt3 in Action</h1>
+    </NuxtLink>
       <Avatar></Avatar>
     </nav>
   </template>
---
+++ pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <UButton @click="onLogin">登录</UButton>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import{useLogin} from '~/composable/user'
+  const isLogin = useLogin()
+  const router = useRouter()
+  const route = useRoute()
+  const onLogin = () => {
+    isLogin.value = true
+    const callback= route.query.callback?.toString() || ''
+    router.push(callback)
+  }
+  </script>
+  
